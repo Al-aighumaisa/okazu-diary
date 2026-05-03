44806bb16e1f0ffd5e7cc9d99edc743e081c5c9d
مرزم اليَغميصاء <al.aighumaisa@gmail.com>
2026-05-04 06:56:59 +0900

    web: Clean up root module

--- packages/web/app/root.tsx
+++ packages/web/app/root.tsx
@@ -1,3 +1,4 @@
+import 'react-loading-skeleton/dist/skeleton.css';
 import {
   isRouteErrorResponse,
   Links,
@@ -23,6 +24,49 @@ export function Layout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icon.svg" sizes="any" type="image/svg+xml" />
+        <link
+          rel="icon"
+          href="/icon-192.png"
+          sizes="192x192"
+          type="image/png"
+        />
+        <link
+          rel="icon"
+          href="/icon-128.png"
+          sizes="128x128"
+          type="image/png"
+        />
+        <link
+          rel="icon apple-touch-icon"
+          href="/apple-touch-icon.png"
+          sizes="1024x1024"
+          type="image/png"
+        />
+        <link
+          rel="icon apple-touch-icon"
+          href="/icon-60@3x.png"
+          sizes="180x180"
+          type="image/png"
+        />
+        <link
+          rel="icon apple-touch-icon"
+          href="/icon-60@2x.png"
+          sizes="120x120"
+          type="image/png"
+        />
+        <link
+          rel="icon apple-touch-icon"
+          href="/icon-167.png"
+          sizes="167x167"
+          type="image/png"
+        />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+          sizes="16x16 32x32 48x48"
+          type="image/vnd.microsoft.icon"
+        />
         <Meta />
         <Links />
       </head>
--- packages/web/app/routes.ts
+++ packages/web/app/routes.ts
@@ -1,23 +1,16 @@
-import {
-  type RouteConfig,
-  index,
-  route,
-  layout,
-} from '@react-router/dev/routes';
+import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
 import { allowed_dids } from './config';
 
 const [did, ...restDids] = allowed_dids;
 
 export default [
-  layout('routes/layout.tsx', [
-    ...(did && !restDids.length
-      ? [
-          index('routes/profile/home.tsx', { id: 'homeProfile' }),
-          route('entry', 'routes/profile/entry.tsx', { id: 'homeEntry' }),
-        ]
-      : [index('routes/home.tsx')]),
-    route(':id', 'routes/profile/home.tsx'),
-    route(':id/entry', 'routes/profile/entry.tsx'),
-  ]),
+  ...(did && !restDids.length
+    ? [
+        index('routes/profile/home.tsx', { id: 'homeProfile' }),
+        route('entry', 'routes/profile/entry.tsx', { id: 'homeEntry' }),
+      ]
+    : [index('routes/home.tsx')]),
+  route(':id', 'routes/profile/home.tsx'),
+  route(':id/entry', 'routes/profile/entry.tsx'),
 ] satisfies RouteConfig;
